5cdc47f5c91229ed012b7db09deca165216c5ebc
borisovvilyaa <borisovvilyaa@gmail.com>
2024-06-18 13:56:50 +0300

    fix

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -224,60 +224,60 @@
       Privacy Policy.
     </p>
 
-    <h3>Use of Your Personal Data</h3>
+    <h2>Use of Your Personal Data</h2>
     <p>The Company may use Personal Data for the following purposes:</p>
     <ul>
       <li>
-        <strong>To provide and maintain our Service</strong>, including to
-        monitor the usage of our Service.
+        To provide and maintain our Service, including to monitor the usage of
+        our Service.
       </li>
       <li>
-        <strong>To manage Your Account:</strong> to manage Your registration as
-        a user of the Service. The Personal Data You provide can give You access
-        to different functionalities of the Service that are available to You as
-        a registered user.
+        To manage Your Account: to manage Your registration as a user of the
+        Service. The Personal Data You provide can give You access to different
+        functionalities of the Service that are available to You as a registered
+        user.
       </li>
       <li>
-        <strong>For the performance of a contract:</strong> the development,
-        compliance and undertaking of the purchase contract for the products,
-        items or services You have purchased or of any other contract with Us
-        through the Service.
+        For the performance of a contract: the development, compliance and
+        undertaking of the purchase contract for the products, items or services
+        You have purchased or of any other contract with Us through the Service.
       </li>
       <li>
-        <strong>To contact You:</strong> To contact You by email, telephone
-        calls, SMS, or other equivalent forms of electronic communication, such
-        as a mobile application's push notifications regarding updates or
-        informative communications related to the functionalities, products or
-        contracted services, including the security updates, when necessary or
-        reasonable for their implementation.
+        To contact You: To contact You by email, telephone calls, SMS, or other
+        equivalent forms of electronic communication, such as a mobile
+        application's push notifications regarding updates or informative
+        communications related to the functionalities, products or contracted
+        services, including the security updates, when necessary or reasonable
+        for their implementation.
       </li>
       <li>
-        <strong>To provide You</strong> with news, special offers and general
-        information about other goods, services and events which we offer that
-        are similar to those that you have already purchased or enquired about
-        unless You have opted not to receive such information.
+        To provide You with news, special offers and general information about
+        other goods, services and events which we offer that are similar to
+        those that you have already purchased or enquired about unless You have
+        opted not to receive such information.
       </li>
       <li>
-        <strong>To manage Your requests:</strong> To attend and manage Your
-        requests to Us.
+        To manage Your requests: To attend and manage Your requests to Us.
       </li>
       <li>
-        <strong>For business transfers:</strong> We may use Your information to
-        evaluate or conduct a merger, divestiture, restructuring,
-        reorganization, dissolution, or other sale or transfer of some or all of
-        Our assets, whether as a going concern or as part of bankruptcy,
-        liquidation, or similar proceeding, in which Personal Data held by Us
-        about our Service users is among the assets transferred.
+        For business transfers: We may use Your information to evaluate or
+        conduct a merger, divestiture, restructuring, reorganization,
+        dissolution, or other sale or transfer of some or all of Our assets,
+        whether as a going concern or as part of bankruptcy, liquidation, or
+        similar proceeding, in which Personal Data held by Us about our Service
+        users is among the assets transferred.
       </li>
       <li>
-        <strong>For other purposes:</strong> We may use Your information for
-        other purposes, such as data analysis, identifying usage trends,
-        determining the effectiveness of our promotional campaigns and to
-        evaluate and improve our Service, products, services, marketing and your
-        experience.
+        For other purposes: We may use Your information for other purposes, such
+        as data analysis, identifying usage trends, determining the
+        effectiveness of our promotional campaigns and to evaluate and improve
+        our Service, products, services, marketing and your experience.
       </li>
     </ul>
-    <p>We may share Your personal information in the following situations:</p>
+    <p>
+      Any in-game purchase is the property of the developers and only the
+      developers.
+    </p>
     <ul>
       <li>
         <strong>With Service Providers:</strong> We may share Your personal
